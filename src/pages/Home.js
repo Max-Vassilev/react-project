b0945e23353dc686a71783c7b139b9d58cbdee97
Maxim Vassilev <maxim@Maxims-MacBook-Air.local>
2026-01-22 16:19:11 +0200

    Personalize project - minimal changes

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import "../styles/home.css";
+
+function Home() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Welcome 👋</h1>
+        <p>Example React project</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
