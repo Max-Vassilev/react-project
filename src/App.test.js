32d14757314cbe25a1d04217760a3027591e6c56
Maxim Vassilev <maxim@Mac.localstaff>
2026-01-22 16:42:02 +0200

    Fix test file so it passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders example react project text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/example react project/i);
+  expect(textElement).toBeInTheDocument();
 });
