38a679b0beed4f95d79ab5f34ba0ce88f5350a56
Maxim Vassilev <maxim@Mac.localstaff>
2026-01-22 16:46:27 +0200

    Fix test file so it passes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders Example React project text", () => {
   render(<App />);
-  const textElement = screen.getByText(/Example React project/);
+  const textElement = screen.getByText(/Example react project/);
   expect(textElement).toBeInTheDocument();
 });
